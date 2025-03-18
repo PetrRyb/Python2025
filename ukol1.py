d435012cff3f5cefd7c4a2d9dbc99a07d39d4421
PetrRyb <petr.rybin@infis.cz>
2025-03-18 09:40:31 +0100

    Add files via upload

---
+++ ukol1.py
@@ -0,0 +1,3 @@
+věk = int(input("Kolik je ti let?")):
+jméno = str(input("Jaké je vaše jméno?")):
+print(f"Ahoj jmenuji se {jméno} a je mi {věk} let.")
---
+++ ukol2.py
@@ -0,0 +1,2 @@
+desetinnéčíslo = int(input("Jaké desetinné číslo bys sem chtěl zadat?"))
+desetinnéčíslo2 = int
