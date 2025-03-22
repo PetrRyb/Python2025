7ac1c36da1760bf093d3ee70da34ca12c43f2c38
PetrRyb <petr.rybin@infis.cz>
2025-03-22 02:02:43 +0100

    Update ukol2.py

--- ukol2.py
+++ ukol2.py
@@ -1,2 +1,10 @@
-desetinnéčíslo = int(input("Jaké desetinné číslo bys sem chtěl zadat?"))
-desetinnéčíslo2 = int
+desetinnéčíslo = int(input("Jaké desetinné číslo chcete zadat?"))
+desetinnéčíslo2 = int(input("Jaké druhé desetinné číslo chcete zadat?"))
+součet = desetinnéčíslo + desetinnéčíslo2
+rozdíl = desetinnéčíslo - desetinnéčíslo2
+součin = desetinnéčíslo * desetinnéčíslo2
+podíl = desetinnéčíslo / desetinnéčíslo2
+print(f"Součet:{součet:.2f}")
+print(f"Rozdíl: {rozdíl:.2f}")
+print(f"Součin: {součin:.2f}")
+print(f"Podíl: {podíl:.2f}")
